29bf48c9fd3b0e99afa10855043964cf6ed08c57
Miroslav Rachnev <mrachnev@yahoo.com>
2025-10-13 15:39:56 +0300

    Add project files.

---
+++ TransactionSystem.Api.Tests.Unit/AccountsControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using TransactionSystem.Api.Controllers;
+
+namespace TransactionSystem.Api.Tests.Unit
+{
+    public class AccountsControllerTests
+    {
+        [Fact]
+        public void GetAccountsOk()
+        {
+            var controller = new AccountsController();
+
+            var result = controller.GetAccounts() as OkObjectResult;
+
+            var accounts = result?.Value as string[];
+
+            accounts.Should().BeEquivalentTo(new[] { "Account1", "Account2", "Hello World" });
+        }
+    }
+}
---
+++ TransactionSystem.Api/Controllers/AccountsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TransactionSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAccounts()
+        {
+            // Placeholder for getting accounts logic
+            return Ok(new[] { "Account1", "Account2", "Hello World" });
+        }
+    }
+}
