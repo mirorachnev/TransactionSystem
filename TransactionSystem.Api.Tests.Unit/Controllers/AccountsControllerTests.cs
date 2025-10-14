0c81c51ffb1599aa4d6df885807e4fded2974d7c
Miroslav Rachnev <mrachnev@yahoo.com>
2025-10-14 16:15:17 +0300

    Add more tests

--- TransactionSystem.Api.Tests.Unit/Controllers/AccountsControllerTests.cs
+++ TransactionSystem.Api.Tests.Unit/Controllers/AccountsControllerTests.cs
@@ -1,21 +1,197 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using TransactionSystem.Api.Controllers;
+using TransactionSystem.Api.Repositories;
+using TransactionSystem.Api.Repositories.Models;
 
 namespace TransactionSystem.Api.Tests.Unit.Controllers
 {
     public class AccountsControllerTests
     {
-        [Fact]
-        public void GetAccountsOk()
+        private readonly Mock<IAccountsRepository> _accountsRepositoryMock;
+        private readonly AccountsController _accountsController;
+
+        public AccountsControllerTests()
         {
-            //var controller = new AccountsController();
+            _accountsRepositoryMock = new Mock<IAccountsRepository>();
+            _accountsController = new AccountsController(_accountsRepositoryMock.Object);
+        }
 
-            //var result = controller.GetAccounts() as OkObjectResult;
+        [Fact]
+        public async Task GetAllAccountsAsyncOkTest()
+        {
+            var accounts = new[]
+             {
+                new AccountData { AccountId = "1", Balance = 100, Name = "Test" },
+                new AccountData { AccountId = "2", Balance = 200, Name = "Test" },
+                new AccountData { AccountId = "3", Balance = 300, Name = "Test" }
+            };
 
-            //var accounts = result?.Value as string[];
+            _accountsRepositoryMock.Setup(repo => repo.GetAllAccountsAsync())
+                .ReturnsAsync(accounts);
 
-            //accounts.Should().BeEquivalentTo(new[] { "Account1", "Account2", "Hello World" });
+            var result = await _accountsController.GetAllAccountsAsync();
+
+            var objectResult = result.Result as OkObjectResult;
+            var resultAccounts = objectResult?.Value as IEnumerable<AccountData>;
+
+            resultAccounts.Should().BeEquivalentTo(accounts);
+        }
+
+        [Fact]
+        public async Task GetAllAccountsAsyncOkEmptyDataTest()
+        {
+            _accountsRepositoryMock.Setup(repo => repo.GetAllAccountsAsync())
+                .ReturnsAsync([]);
+
+            var result = await _accountsController.GetAllAccountsAsync();
+
+            var objectResult = result.Result as OkObjectResult;
+            var resultAccounts = objectResult?.Value as IEnumerable<AccountData>;
+
+            resultAccounts.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetAccountByIdAsyncOkTest()
+        {
+            var account = new AccountData { AccountId = "1", Balance = 100, Name = "Test" };
+            _accountsRepositoryMock.Setup(repo => repo.GetAccountByIdAsync("1"))
+                .ReturnsAsync(account);
+            var result = await _accountsController.GetAccountByIdAsync("1");
+            var objectResult = result.Result as OkObjectResult;
+            var resultAccount = objectResult?.Value as AccountData;
+            resultAccount.Should().BeEquivalentTo(account);
+        }
+
+        [Fact]
+        public async Task GetAccountByIdAsyncNotFoundTest()
+        {
+            _accountsRepositoryMock.Setup(repo => repo.GetAccountByIdAsync("nonexistent"))
+                .ReturnsAsync((AccountData?)null);
+            var result = await _accountsController.GetAccountByIdAsync("nonexistent");
+            var notFoundResult = result.Result as NotFoundResult;
+            notFoundResult.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task CreateAccountAsyncOkTest()
+        {
+            var account = new AccountData { AccountId = "1", Balance = 100, Name = "Test" };
+            _accountsRepositoryMock.Setup(repo => repo.AddAccountAsync(account))
+                .ReturnsAsync(true);
+            var result = await _accountsController.CreateAccountAsync(account);
+            var objectResult = result.Result as OkObjectResult;
+            var resultAccount = objectResult?.Value as AccountData;
+            resultAccount.Should().BeEquivalentTo(account);
+        }
+
+        [Fact]
+        public async Task CreateAccountAsyncBadRequestTest()
+        {
+            var account = new AccountData { AccountId = "1", Balance = -100, Name = "Test" };
+            var result = await _accountsController.CreateAccountAsync(account);
+            var badRequestResult = result.Result as BadRequestObjectResult;
+            badRequestResult.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task CreateAccountAsyncInternalServerErrorTest()
+        {
+            var account = new AccountData { AccountId = "1", Balance = 100, Name = "Test" };
+            _accountsRepositoryMock.Setup(repo => repo.AddAccountAsync(account))
+                .ReturnsAsync(false);
+            var result = await _accountsController.CreateAccountAsync(account);
+            var objectResult = result.Result as ObjectResult;
+            objectResult.Should().NotBeNull();
+            objectResult?.StatusCode.Should().Be(500);
+        }
+
+        [Fact]
+        public async Task RemoveAccountAsyncOkTest()
+        {
+            _accountsRepositoryMock.Setup(repo => repo.RemoveAccountAsync("1"))
+                .ReturnsAsync(true);
+            var result = await _accountsController.RemoveAccountAsync("1");
+            var okResult = result as OkResult;
+            okResult.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task RemoveAccountAsyncInternalServerErrorTest()
+        {
+            _accountsRepositoryMock.Setup(repo => repo.RemoveAccountAsync("1"))
+                .ReturnsAsync(false);
+            var result = await _accountsController.RemoveAccountAsync("1");
+            var objectResult = result as ObjectResult;
+            objectResult.Should().NotBeNull();
+            objectResult?.StatusCode.Should().Be(500);
+        }
+
+        [Fact]
+        public async Task RemoveAccountAsyncNotFoundTest()
+        {
+            _accountsRepositoryMock.Setup(repo => repo.RemoveAccountAsync("nonexistent"))
+                .ReturnsAsync(false);
+            var result = await _accountsController.RemoveAccountAsync("nonexistent");
+            var objectResult = result as ObjectResult;
+            objectResult.Should().NotBeNull();
+            objectResult?.StatusCode.Should().Be(500);
+        }
+
+        [Fact]
+        public async Task DepositMoneyAsyncOkTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task DepositMoneyAsyncNotFoundTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task DepositMoneyAsyncNegativeAmountTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task WithdrawMoneyAsyncOkTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task WithdrawMoneyAsyncNotFoundTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task WithdrawMoneyAsyncNegativeAmountTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task TransferMoneyAsyncOkTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task TransferMoneyAsyncNotFoundTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task TransferMoneyAsyncNegativeAmountTest()
+        {
+
         }
     }
 }
