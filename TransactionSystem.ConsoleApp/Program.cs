57a6c7c6931cb48859f01491f8c197789cbfe7a4
Miroslav Rachnev <mrachnev@yahoo.com>
2025-10-16 11:02:54 +0300

    Updates

--- TransactionSystem.ConsoleApp/Program.cs
+++ TransactionSystem.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TransactionSystem.DataAccess.Repositories;
+using TransactionSystem.DataAccess.Repositories.Models;
 
 namespace TransactionSystem.ConsoleApp
 {
@@ -12,16 +13,20 @@ namespace TransactionSystem.ConsoleApp
 
             var builder = Host.CreateApplicationBuilder(args);
 
-            builder.Configuration.Sources.Clear();
-            IHostEnvironment env = builder.Environment;
-
             builder.Services.AddSingleton<IAccountsRepository, AccountsRepository>();
 
             using var serviceProvider = builder.Services.BuildServiceProvider();
 
             var repositoryService = serviceProvider.GetRequiredService<IAccountsRepository>();
 
+            await repositoryService.AddAccountAsync(new AccountData { AccountId = "1", Name = "test", Balance = 100 });
+
             var accounts = await repositoryService.GetAllAccountsAsync();
+
+            foreach (var account in accounts) 
+            {
+                Console.WriteLine($"Account id - {account.AccountId}, Name - {account.Name}, balance - {account.Balance}");
+            }
 
             Console.ReadKey();
 
